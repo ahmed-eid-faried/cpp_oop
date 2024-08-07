ca512fa73b852f95b28578608c9ec8de37b2eb30
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-08-07 13:19:27 +0300

    setters and getters

--- cpp_oop/cpp_oop.cpp
+++ cpp_oop/cpp_oop.cpp
@@ -11,12 +11,7 @@ private:
 
 public:
 
-	clsPerson(
-		int Id,
-		string FristName,
-		string LastName,
-		string Email,
-		string Phone)
+	clsPerson(int Id, string FristName, string LastName, string Email, string Phone)
 	{
 		_Id = Id;
 		_FristName = FristName;
@@ -52,11 +47,59 @@ public:
 	~clsPerson() {
 		cout << "Hello World,I'm destrator.\n";
 	}
+	//properties ==>> setters and getters:-
+	//Id
+	int GetId() { return _Id; }
+	__declspec(property(get = GetId))int Id;
+	//FristName
+	string GetFristName() { return _FristName; }
+	void SetFristName(string FristName) { _FristName = FristName; }
+	__declspec(property(get = GetFristName, put = SetFristName))string FristName;
+	//LastName
+	string GetLastName() { return _LastName; }
+	void SetLastName(string LastName) { _LastName = LastName; }
+	__declspec(property(get = GetLastName, put = SetLastName))string LastName;
+	//FullName
+	string GetFullName() { return _FullName; }
+	__declspec(property(get = GetFullName))string FullName;
+	//Email
+	string GetEmail() { return _Email; }
+	void SetEmail(string Email) { _Email = Email; }
+	__declspec(property(get = GetEmail, put = SetEmail))string Email;
+	//Phone
+	string GetPhone() { return _Phone; }
+	void SetPhone(string Phone) { _Phone = Phone; }
+	__declspec(property(get = GetPhone, put = SetPhone))string Phone;
+
 };
 int main()
 {
 	clsPerson clsPerson(1, "Ahmed", "Mady", "test@gmail.com", "0121321424");
 	clsPerson.Print();
+
 	clsPerson.SendEmail("Hi, Ahmed", "How are you?");
+
 	clsPerson.SendSMS("Hi, Ahmed");
+
+	//clsPerson.Id = 1;
+	cout << clsPerson.Id << endl;
+
+	clsPerson.FristName = "AAAAAAA";
+	cout << clsPerson.FristName << endl;
+
+	clsPerson.LastName = "MMMMMM";
+	cout << clsPerson.LastName << endl;
+
+	//clsPerson.FullName = "AAAAAAA MMMMMM";
+	cout << clsPerson.FullName << endl;
+
+	clsPerson.Email = "a@gmail.com";
+	cout << clsPerson.Email << endl;
+
+	clsPerson.Phone = "01555544555";
+	cout << clsPerson.Phone << endl;
+
+	cout <<  endl;
+	clsPerson.Print();
+
 }
